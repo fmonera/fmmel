cc1427e1960676592271fbec568826b43d6b98e5
Fernando Monera <fmonera@gmail.com>
2023-01-06 00:15:57 +0100

    Docker registry instead of Github registry.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,6 +4,6 @@
 #####
 # You need to be logged in to the registry to upload.
 
-docker build -t ghcr.io/fmonera/fmmel:latest .
-docker push ghcr.io/fmonera/fmmel:latest
+docker build -t fmonera/fmmel:latest .
+docker push fmonera/fmmel:latest
 
